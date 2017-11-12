fa853b38fecaf85c134ca3e1b1e4c5444aaea40c
Mário Fernandes <mgustavofernandes@gmail.com>
2017-11-12 11:42:15 +0000

    referencia + post contacto

--- server/Lib_Primavera/PriIntegrationContacto.cs
+++ server/Lib_Primavera/PriIntegrationContacto.cs
@@ -1,5 +1,6 @@
 ﻿using FirstREST.Lib_Primavera.Model;
 using Interop.StdBE900;
+using Interop.CrmBE900;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -54,7 +55,52 @@ namespace FirstREST.Lib_Primavera
 
         internal static RespostaErro InsereContactoObj(Contacto contacto)
         {
-            throw new NotImplementedException();
+            Lib_Primavera.Model.RespostaErro erro = new Model.RespostaErro();
+
+
+            CrmBEContacto myContact = new CrmBEContacto();
+
+            try
+            {
+                if (PriEngine.InitializeCompany(FirstREST.Properties.Settings.Default.Company.Trim(), FirstREST.Properties.Settings.Default.User.Trim(), FirstREST.Properties.Settings.Default.Password.Trim()) == true)
+                {
+
+                    myContact.set_PrimeiroNome(contacto.PrimeiroNome);
+                    myContact.set_NomesIntermedios(contacto.NomesIntermedios);
+                    myContact.set_UltimoNome(contacto.UltimoNome);
+                    myContact.set_NumContribuinte(contacto.NumContribuinte);
+                    myContact.set_Morada(contacto.Morada);
+                    myContact.set_CodPostal(contacto.CodPostal);
+                    myContact.set_Localidade(contacto.Localidade);
+                    myContact.set_Pais(contacto.Pais);
+                    myContact.set_Notas(contacto.Notas);
+                    myContact.set_Titulo(contacto.Titulo);
+                    myContact.set_Telefone(contacto.Telefone);
+                    myContact.set_Telemovel(contacto.Telemovel);
+                    myContact.set_Email(contacto.Email);
+                    myContact.set_Contacto(contacto.PrimeiroNome.ToUpper()[0] + contacto.UltimoNome.ToUpper());
+                    myContact.set_ID(Guid.NewGuid().ToString());
+
+                    PriEngine.Engine.CRM.Contactos.Actualiza(myContact);
+
+                    erro.Erro = 0;
+                    erro.Descricao = "Sucesso";
+                    return erro;
+                }
+                else
+                {
+                    erro.Erro = 1;
+                    erro.Descricao = "Erro ao abrir empresa";
+                    return erro;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                erro.Erro = 1;
+                erro.Descricao = ex.Message;
+                return erro;
+            }
         }
     }
 }
