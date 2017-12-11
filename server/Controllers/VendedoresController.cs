ad467ad3c5654dd2d93ff6c0a7a3f98b056b06f0
Mário Fernandes <mgustavofernandes@gmail.com>
2017-12-11 12:22:45 +0000

    update register / login

---
+++ server/Controllers/VendedoresController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FirstREST.Controllers
+{
+    public class VendedoresController : ApiController
+    {
+
+        // GET: /Vendedores/
+        [HttpGet]
+        [Route("api/vendedores")]
+        public IEnumerable<Lib_Primavera.Model.Vendedor> Get()
+        {
+            return Lib_Primavera.PriIntegrationVendedor.ListaVendedores();
+        }
+
+        // POST: /Vendedores/
+        [HttpPost]
+        [Route("api/vendedores")]
+        public HttpResponseMessage Post(Lib_Primavera.Model.Vendedor vendedor)
+        {
+            Lib_Primavera.Model.RespostaErro erro = new Lib_Primavera.Model.RespostaErro();
+            erro = Lib_Primavera.PriIntegrationVendedor.InsereVendedorObj(vendedor);
+
+            if (erro.Erro == 0)
+            {
+                var response = Request.CreateResponse(
+                   HttpStatusCode.Created, vendedor);
+                string uri = Url.Link("DefaultApi", new { Id = vendedor.Id });
+                //response.Headers.Location = new Uri(uri);
+                return response;
+            }
+
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+        }
+
+        // POST: /Vendedores/login
+        [HttpPost]
+        [Route("api/vendedores/login")]
+        public HttpResponseMessage Login(Lib_Primavera.Model.Vendedor vendedor)
+        {
+            Lib_Primavera.Model.RespostaErro erro = new Lib_Primavera.Model.RespostaErro();
+            erro = Lib_Primavera.PriIntegrationVendedor.CheckLoginDetails(vendedor);
+
+            if (erro.Erro >= 0)
+            {
+                Lib_Primavera.Model.Vendedor myVendedor = Lib_Primavera.PriIntegrationVendedor.GetVendedor(erro.Erro);
+                var response = Request.CreateResponse(
+                   HttpStatusCode.Accepted, myVendedor);
+                string uri = Url.Link("DefaultApi", new { Id = vendedor.Id });
+                //response.Headers.Location = new Uri(uri);
+                return response;
+            }
+
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+        }
+
+    }
+}
---
+++ server/Lib_Primavera/Model/Vendedor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstREST.Lib_Primavera.Model
+{
+    public class Vendedor
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Morada { get; set; }
+        public string Localidade { get; set; }
+        public string CodPostal { get; set; }
+        public string Telefone { get; set; }
+        public string Telemovel { get; set; }
+        public string Email { get; set; }
+        public string Fax { get; set; }
+        public string Notas { get; set; }
+    }
+}
