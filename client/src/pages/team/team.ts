20d79fac0f3b7ac5fc2f0b653ea22accb27181c3
Mário Fernandes <mgustavofernandes@gmail.com>
2017-12-13 17:58:12 +0000

    core 5 - team

--- client/src/pages/team/team.ts
+++ client/src/pages/team/team.ts
@@ -1,44 +1,96 @@
-import { NavController } from 'ionic-angular';
-import { ChangeDetectionStrategy, Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { CalendarEvent } from 'angular-calendar';
-import { Input, Output, EventEmitter } from '@angular/core';
-import { getDateString } from "../calendar-resources";
-import { window } from "rxjs/operator/window";
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from '@angular/core';
+import { VendorsProvider } from '../../providers/vendors/vendors';
+import { NativeStorage } from '@ionic-native/native-storage';
 
 
 @Component({
   selector: 'page-team',
   templateUrl: 'team.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TeamPage {
 
-  private teamChefe = { name: "Lydia Sun" };
-  private teamMembers = [];
+  private teamChefe = {Nome: ""}
+  public teamMembers = [];
 
-  constructor() {
-
+  constructor(public navCtrl: NavController, public navParams: NavParams, private vendorProvider: VendorsProvider, private nativeStorage: NativeStorage) {
   }
 
   ionViewDidLoad() {
-    this.teamMembers = [
-      [
-        { name: "John Valley" },
-        { name: "Lydia Sun" },
-        { name: "John Valley" },
-        { name: "Lydia Sun" },
-        { name: "John Valley" }
-      ],
-      [
-        { name: "John Valley" },
-        { name: "Lydia Sun" },
-        { name: "John Valley" },
-        { name: "Lydia Sun" },
-        { name: "John Valley" }
-      ],
-    ];
+
+    this.nativeStorage.getItem("Role").then(
+      role => {
+        this.nativeStorage.getItem("Id").then(
+          id => {
+            this.nativeStorage.getItem("Nome").then(
+              nome => {
+                if (role == "Chefe") {
+                  this.teamChefe = { Nome: nome };
+                  this.vendorProvider.getChiefTeam(id).subscribe(
+                    data => {
+                      while (data.length > 0)
+                        this.teamMembers.push(data.splice(0, 4));
+                    },
+                    err => {
+
+                    });
+
+                } else if (role == "Vendedor") {
+
+                  this.vendorProvider.getVendorChief(id).subscribe(
+                    data => {
+                      if (data.length > 0) {
+                        this.teamChefe = data[0];
+                      }
+                    },
+                    err => {
+
+                    });
+
+                  this.teamMembers.push({ Nome: nome });
+
+                  this.vendorProvider.getVendorTeam(id).subscribe(
+                    data => {
+                      let start = 1;
+                      while (data.length > 0) {
+                        if (start == 1) {
+                          this.teamMembers.push(data.splice(0, 3));
+                          start++;
+                        } else {
+                          this.teamMembers.push(data.splice(0, 4));
+                        }
+                      }
+                    },
+                    err => {
+
+                    });
+
+
+                } else {
+                  alert("Please Login!");
+                }
+
+
+
+
+              });
+
+          }
+        );
+
+
+
+
+      },
+      error => {
+
+      }
+    )
+
+
 
   }
+
 
 }
 
