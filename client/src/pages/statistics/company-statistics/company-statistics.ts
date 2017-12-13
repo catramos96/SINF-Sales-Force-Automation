3a4e3fda67ba6def306c000fb3433a36dd7f9f76
Tiago Filipe <tiagoperolafilipe@gmail.com>
2017-12-13 21:32:39 +0000

    Core das Estatísticas melhorado por completo.

--- client/src/pages/statistics/company-statistics/company-statistics.ts
+++ client/src/pages/statistics/company-statistics/company-statistics.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { StatisticsProvider } from '../../../providers/statistics/statistics';
+import { ModalContentPage } from '../../product/productModal';
+import Chart from 'chart.js';
 
 @IonicPage()
 @Component({
@@ -12,15 +14,37 @@ export class CompanyStatisticsPage {
   private soldProductsNumber: number;
   private billedMoneyNumber: number;
   private top5Products: JSON[] = [];
-  private productsSoldByCategory: JSON[] = [];
+  private top5Images: string[] = ["assets/imgs/logo.png"];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private statisticsP: StatisticsProvider) {
+  private pieChartLabels: string[] = [];
+  private pieChartData: number[] = [];
+  private pieChartType: string = "pie";
+
+  private colorsChart: string[] = [
+    'rgba(255, 99, 132, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(255, 206, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)',
+    'rgba(153, 102, 255, 0.2)',
+    'rgba(255, 159, 64, 0.2)',
+    'rgba(121, 159, 64, 0.2)',
+    'rgba(121, 159, 206, 0.2)',
+    'rgba(239, 168, 213, 0.2)',
+    'rgba(54, 89, 62, 0.2)',
+    'rgba(249, 89, 62, 0.2)',
+    'rgba(249, 236, 62, 0.2)',
+    'rgba(249, 236, 179, 0.2)',
+    'rgba(11, 123, 221, 0.2)',
+    'rgba(138, 139, 140, 0.2)'
+  ];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private statisticsP: StatisticsProvider, public modalCtrl: ModalController) {
   }
 
-  ionViewDidLoad() {
+  ionViewWillEnter() {
     this.statisticsP.getSoldProductsNumber().subscribe(
       data => {
-        this.soldProductsNumber = data;
+        this.soldProductsNumber = data.toFixed(2).toLocaleString('pt-PT');;
       },
       err => {
 
@@ -36,7 +60,13 @@ export class CompanyStatisticsPage {
 
     this.statisticsP.getSoldProductsByCategory().subscribe(
       data => {
-        this.productsSoldByCategory = data;
+        let i;
+        for (i = 0; i < data.length; i++) {
+          this.pieChartData[i] = data[i].Quantidade;
+          this.pieChartLabels[i] = data[i].FamiliaNome.toString();
+        }
+
+        this.createPieChart();
       },
       err => {
 
@@ -44,11 +74,46 @@ export class CompanyStatisticsPage {
 
     this.statisticsP.getBilledMoneyNumber().subscribe(
       data => {
-        this.billedMoneyNumber = data.toFixed(2);
+        this.billedMoneyNumber = data.toFixed(2).toLocaleString('pt-PT');
       },
       err => {
 
       });
+  }
+
+  public createPieChart() {
+    var ctx = document.getElementById("companyChart");
+
+    new Chart(ctx, {
+      type: 'pie',
+      data: {
+        datasets: [{
+          data: this.pieChartData,
+          backgroundColor: this.poolColors(this.pieChartData.length),
+        }],
+        labels: this.pieChartLabels
+      },
+      options: {
+        legend: {
+          display: false
+        }
+      }
+    });
+  }
+
+  private poolColors(length) {
+    let colors = [];
+    let i;
+    for (i = 0; i < this.colorsChart.length; i++) {
+      colors[i] = this.colorsChart[i];
+    }
+
+    return colors;
+  }
+
+  openModal(productID) {
+    let modal = this.modalCtrl.create(ModalContentPage, { productID: productID });
+    modal.present();
   }
 
 }
